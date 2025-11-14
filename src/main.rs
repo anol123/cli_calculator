35ca7b82ef4addd11e0100326315b6d8b0005a0c
anol123 <anol.roy123@gmail.com>
2025-11-14 21:55:38 +0530

    Error handling added

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,43 @@ enum CalError{
     InvalidOperator,
 }
 fn main() {
-    let num1 =2.0;
-    let num2 =3.0;
-    let op = "+";
+    loop{
+        let input =read_input("Enter your choice: compute or exit");
+        if input =="exit".to_string(){
+            println!("Goodbye!!!");
+            return;
+        }
+        else{
+            let input1 = read_input("Enter first number");
 
-    match compute(num1, num2, op){
-        Ok(result) =>  println!("Result:  {result}"),
-        Err(CalError::DivisionByZero) => println!("Cannot divide by zero"),
-        Err(CalError::InvalidOperator) => println!("Please enter a valid operator"),
-        Err(CalError::InvalidNumber) => println!("Invalid input entered")
+            let num1 = match parse_number(input1){
+                Ok(input) =>input,
+                Err(_) =>{
+                    println!("Invalid number");
+                    continue;
+                } 
+            };
+
+            let num2 = match parse_number( read_input("Enter second number")){
+                Ok(input) =>input,
+                Err(_) =>{
+                    println!("Invalid number");
+                    continue;
+                } 
+            };
+
+            let op =read_input("Enter operator (+, -, *, /)");
+        
+        match compute(num1, num2, &op){
+            Ok(result) =>  println!("Result:  {result}"),
+            Err(CalError::DivisionByZero) => println!("Cannot divide by zero"),
+            Err(CalError::InvalidOperator) => println!("Please enter a valid operator"),
+            Err(CalError::InvalidNumber) => println!("Invalid input entered")
+        };
     }
-   
+        
+     
+    }
 }
 
 fn compute(num1:f64, num2:f64, op:&str)-> Result<f64, CalError>{
@@ -33,4 +59,15 @@ fn compute(num1:f64, num2:f64, op:&str)-> Result<f64, CalError>{
         _ => Err(CalError::InvalidOperator),
     };
     return result;
+}
+
+fn read_input(prompt:&str)-> String{
+    println!("{}", prompt);
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read");
+    input.trim().to_string()
+}
+
+fn parse_number(input:String) -> Result<f64, CalError>{
+    input.trim().parse::<f64>().map_err(|_| CalError::InvalidNumber)
 }
